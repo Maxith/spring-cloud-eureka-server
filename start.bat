fa692e0b42989b5b4819299e1753ef9ed80cd272
Maxith <zhouyou457@hotmail.com>
2017-11-29 11:12:41 +0800

    批处理命令

---
+++ start.bat
@@ -0,0 +1,3 @@
+start cmd /c java -jar spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer1
+start cmd /c java -jar spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer2
+start cmd /c java -jar spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer3
